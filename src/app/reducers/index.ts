e9931edf9487af9cf2f6f0019704a81bc4e677cc
Tomas Kneitas <Tomas Kneitas@BSSIT.local>
2019-01-22 17:30:31 +0200

    'push'

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -6,13 +6,34 @@ import {
   MetaReducer
 } from '@ngrx/store';
 import { environment } from '../../environments/environment';
+import { AuthActions, Login, AuthActionTypes } from '../auth/auth.actions';
+
+
+ interface AuthState {
+  loggedIn: boolean;
+  user;
+}
+
+
 
 export interface AppState {
+  auth: AuthState;
+}
 
+function authReducer(state: AuthState, action: AuthActions): AuthState {
+  switch (action.type) {
+    case AuthActionTypes.LoginAction:
+      return {
+        loggedIn: true,
+        user: action.payload
+      };
+      default:
+      return state;
+  }
 }
 
 export const reducers: ActionReducerMap<AppState> = {
-
+auth: authReducer,
 };
 
 
